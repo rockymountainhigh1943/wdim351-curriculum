8337b012d93ac69f9eff63248d0b3f53e7b6d310
Jake Love <rockymountainhigh1943@gmail.com>
2013-01-10 20:49:12 -0800

    Add an author/email comment

--- week_01/git_tutorial/Hello/hello.php
+++ week_01/git_tutorial/Hello/hello.php
@@ -1,7 +1,10 @@
 <?php
+/*
+** Author: Jon Doe (jdoe@doeworks.com)
+*/
 
 echo 'Hello World! Glad to be here.';
 
-$name = $_GET['name'] ? $_GET['name'] : 'Jon Doe';
+$name = $_GET['name'] || 'Jon Doe';
 
 ?>
